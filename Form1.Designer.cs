048291ecf2fc16c2f622ae519a42605388d09de0
Jawdatmohammad <97316232+Jawdatmohammad@users.noreply.github.com>
2022-01-24 21:00:24 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace log_in
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbutton = new System.Windows.Forms.Button();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnbutton
+            // 
+            this.btnbutton.Location = new System.Drawing.Point(100, 159);
+            this.btnbutton.Name = "btnbutton";
+            this.btnbutton.Size = new System.Drawing.Size(84, 29);
+            this.btnbutton.TabIndex = 0;
+            this.btnbutton.Text = "log in";
+            this.btnbutton.UseVisualStyleBackColor = true;
+            this.btnbutton.Click += new System.EventHandler(this.btnbutton_Click);
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(100, 36);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(104, 20);
+            this.txtuser.TabIndex = 1;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(100, 92);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(103, 20);
+            this.txtpass.TabIndex = 2;
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(22, 39);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(54, 13);
+            this.username.TabIndex = 3;
+            this.username.Text = "username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.btnbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbutton;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace log_in
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbutton_Click(object sender, EventArgs e)
+        {
+            string x, a;
+            x = txtuser.Text;
+            a = txtpass.Text;
+            if (x == "jawad" && a == "12345")
+            {
+                Form2 frm2 = new Form2();
+                frm2.Show();
+                this.Hide();
+                txtuser.Focus();
+            }
+            else
+            {
+                MessageBox.Show("invalid username or password");
+                txtuser.Text = "";
+                txtpass.Text = "";
+
+            }
+        }
+    }
+}
